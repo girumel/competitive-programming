6bc06065bb5376af48c9d08f98a35d37b1754b6d
Girum Timerga <girumtim@gmail.com>
2023-07-15 20:58:06 +0300

    Time: 62 ms (65.37%), Space: 16.9 MB (76.82%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        phrase = ""
+        for char in s:
+            if char.isalnum():
+                phrase += char.lower()
+        return phrase == phrase[::-1]
