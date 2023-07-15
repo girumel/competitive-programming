27cbe59066d4d21ea3c53b034cc3903eb1a0aa4c
Girum Timerga <girumtim@gmail.com>
2023-07-15 21:59:14 +0300

    Time: 126 ms (5.11%), Space: 17.2 MB (53.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        phrase = ""
-        for char in s:
-            if char.isalnum():
-                phrase += char.lower()
-        return phrase == phrase[::-1]
+        l, r = 0, len(s) -1
+        alphanums = []
+        alphanums.extend(range(48, 58))
+        alphanums.extend(range(65, 91))
+        alphanums.extend(range(97, 123))
+        
+        while l < r:
+            while l < r and ord(s[l]) not in alphanums:
+                l += 1
+            while r > l and ord(s[r]) not in alphanums:
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
