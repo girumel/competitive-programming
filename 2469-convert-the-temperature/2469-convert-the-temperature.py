9013213717945589404c2648db609a264ed2e706
Girum Timerga <girumtim@gmail.com>
2024-02-01 22:27:55 +0300

    Time: 37 ms (54.64%), Space: 16.6 MB (52.62%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -1,4 +1,4 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        ans = [celsius + 273.15, (celsius * 1.80) + 32.00]
-        return ans
+        answer = [celsius + 273.15, (celsius * (9/5)) + 32]
+        return answer
