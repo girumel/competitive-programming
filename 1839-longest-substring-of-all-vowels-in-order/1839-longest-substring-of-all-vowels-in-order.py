ba8a2df0bdcc2e0a3fa35dfddd97af4a84251012
Girum Timerga <girumtim@gmail.com>
2024-03-30 17:17:43 +0300

    Time: 1645 ms (5.24%), Space: 22.3 MB (51.15%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -4,17 +4,16 @@ class Solution:
         if n < 5:
             return 0
 
-        vowels = ['a', 'e', 'i', 'o', 'u']
-        vowel_index = {v: i for i, v in enumerate(vowels)}
-        counts = [0]*5
+        vowels = "aeiou"
+        counts = [0] * 5
 
         longest = left = 0
         for right in range(n):
-            if word[right] in vowel_index:
-                if right > 0 and vowel_index[word[right]] < vowel_index[word[right - 1]]:
-                    counts = [0]*5
+            if word[right] in vowels:
+                if right > 0 and vowels.index(word[right]) < vowels.index(word[right - 1]):
+                    counts = [0] * 5
                     left = right
-                counts[vowel_index[word[right]]] += 1
+                counts[vowels.index(word[right])] += 1
                 
                 if all(counts):
                     longest = max(longest, right - left + 1)
