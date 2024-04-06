ffd3529e29c3844b1bc7db4c40e0481b45ba5c51
Girum Timerga <girumtim@gmail.com>
2024-04-07 00:58:51 +0300

    Created 'Car Pooling' by A2SV Hub

---
+++ 06-Apr-2024/Car Pooling 117076.py
@@ -0,0 +1,17 @@
+# Problem: Car Pooling - https://leetcode.com/problems/car-pooling/
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        location = []
+
+        for trip in trips:
+            location.append((trip[1], trip[0]))
+            location.append((trip[2], -trip[0]))
+        location.sort()
+
+        for time, passengers in location:
+            capacity -= passengers
+            if capacity < 0:
+                return False
+
+        return True
