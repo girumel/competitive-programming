4d65f498376f7001f7eb95dc931131145d4b6198
Girum Timerga <girumtim@gmail.com>
2024-04-13 23:24:34 +0300

    Created 'Books' by A2SV Hub

---
+++ 13-Apr-2024/Books 122330.py
@@ -0,0 +1,16 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+i, j, total, max_books = 0, 0, 0, 0
+
+while j < n:
+    total += a[j]
+    while total > t:
+        total -= a[i]
+        i += 1
+    max_books = max(max_books, j - i + 1)
+    j += 1
+
+print(max_books)
