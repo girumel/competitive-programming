337b157b05ed2df6f1ad55a0c7aa69c7d3d78d03
Girum Timerga <girumtim@gmail.com>
2024-04-27 23:19:52 +0300

    Created 'B - Mattia Binotto' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 27-Apr-2024/B - Mattia Binotto 135173.py
@@ -0,0 +1,14 @@
+# Problem: B - Mattia Binotto - https://codeforces.com/gym/520098/problem/B
+
+n = int(input())
+t = list(map(int, input().split()))
+t.sort()
+wait = 0
+count = 0
+
+for i in range(n):
+    if wait <= t[i]:
+        count += 1
+        wait += t[i]
+
+print(count)
