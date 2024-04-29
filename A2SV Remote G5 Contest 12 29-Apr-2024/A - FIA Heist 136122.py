ea25f9305cccd68e35875c32cffd2793588cfe13
Girum Timerga <girumtim@gmail.com>
2024-04-29 22:14:12 +0300

    Created 'A - FIA Heist' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 29-Apr-2024/A - FIA Heist 136122.py
@@ -0,0 +1,12 @@
+# Problem: A - FIA Heist - https://codeforces.com/gym/520098/problem/A
+
+string = input()
+stack = []
+
+for char in string:
+    if char == "<" and stack:
+        stack.pop()
+    elif char not in ("<", "-"):
+        stack.append(char)
+
+print("".join(stack))
