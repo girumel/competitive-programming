6b6c5fbfa4a3f58fd7a3a2524f0143c4545bf423
Girum Timerga <girumtim@gmail.com>
2024-04-29 22:58:36 +0300

    Created 'C - Bono, Tyres Are Gone' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 29-Apr-2024/C - Bono, Tyres Are Gone 136181.py
@@ -0,0 +1,20 @@
+# Problem: C - Bono, Tyres Are Gone - https://codeforces.com/gym/520098/problem/C
+
+n = int(input())
+tire_stack = []
+reordering = 0
+required_tire = 1
+
+for _ in range(2*n):
+    cmd, *args = input().split()
+    if cmd == "add":
+        tire_stack.append(int(args[0]))
+    else:
+        if tire_stack and tire_stack[-1] != required_tire:
+            reordering += 1
+            tire_stack.clear()
+        elif tire_stack:
+            tire_stack.pop()
+        required_tire += 1
+
+print(reordering)
