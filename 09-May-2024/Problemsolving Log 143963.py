eb6d63432646a2f488a8a696dc00cd64d8f601cd
Girum Timerga <girumtim@gmail.com>
2024-05-09 11:37:46 +0300

    Created 'Problemsolving Log' by A2SV Hub

---
+++ 09-May-2024/Problemsolving Log 143963.py
@@ -0,0 +1,22 @@
+# Problem: Problemsolving Log - https://codeforces.com/gym/522997/problem/A
+
+from collections import defaultdict
+
+t = int(input())
+
+difficulty = {chr(i): i - ord("A") + 1 for i in range(ord("A"), ord("Z") + 1)}
+
+for _ in range(t):
+    n = int(input())
+    log = input()
+    count = defaultdict(int)
+    solved = 0
+
+    for problem in log:
+        count[problem] += 1
+
+    for problem, time_spent in count.items():
+        if difficulty[problem] <= time_spent:
+            solved += 1
+
+    print(solved)
