03e02527df96dc466913b2a84d7e386b34807d93
Girum Timerga <girumtim@gmail.com>
2024-06-05 22:52:24 +0300

    Created 'E - Valeriy and Deque' by A2SV Hub

---
+++ A2SV Remote G5 Contest 17 05-Jun-2024/E - Valeriy and Deque 164517.py
@@ -0,0 +1,27 @@
+# Problem: E - Valeriy and Deque - https://codeforces.com/gym/527307/problem/E
+
+from collections import deque
+
+n, q = map(int, input().split())
+nums = deque(map(int, input().split()))
+
+pairs = [0] * n
+smalls = []
+first = nums[0]
+
+for i in range(1, n):
+    pairs[i - 1] = (first, nums[i])
+
+    if first > nums[i]:
+        smalls.append(nums[i])
+    else:
+        smalls.append(first)
+        first = nums[i]
+
+for _ in range(q):
+    m = int(input())
+    if m < n:
+        print(pairs[m - 1][0], pairs[m - 1][1])
+    else:
+        m -= n
+        print(first, smalls[m % (n - 1)])
