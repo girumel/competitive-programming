80ce98f3a324cedbaeb53b870294eb7b1f44f17e
Girum Timerga <girumtim@gmail.com>
2024-06-05 22:51:55 +0300

    Created 'D - Binary Cut' by A2SV Hub

---
+++ A2SV Remote G5 Contest 17 05-Jun-2024/D - Binary Cut 164515.py
@@ -0,0 +1,16 @@
+# Problem: D - Binary Cut - https://codeforces.com/gym/527307/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    count = 1
+    target = 0
+
+    for i in range(1, len(s)):
+        if s[i] != s[i - 1]:
+            count += 1
+        if s[i - 1] == "0" and s[i] == "1":
+            target = 1
+
+    print(count - target)
