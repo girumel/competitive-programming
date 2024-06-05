2d47269c78b89f411fdf34f61ea518ad6962222d
Girum Timerga <girumtim@gmail.com>
2024-06-05 22:52:17 +0300

    Created 'B - Different String' by A2SV Hub

---
+++ A2SV Remote G5 Contest 17 05-Jun-2024/B - Different String 164516.py
@@ -0,0 +1,14 @@
+# Problem: B - Different String - https://codeforces.com/gym/527307/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    for i in range(1, len(s)):
+        if s[i] != s[i - 1]:
+            s = s[: i - 1] + s[i] + s[i - 1] + s[i + 1 :]
+            print("YES")
+            print(s)
+            break
+    else:
+        print("NO")
