6a1070177576cadfd2f6d86f2775d7851835053e
Girum Timerga <girumtim@gmail.com>
2024-06-19 19:25:56 +0300

    Created 'B - Binary Tournament Tree' by A2SV Hub

---
+++ A2SV Remote G5 Contest 19 19-Jun-2024/B - Binary Tournament Tree 169970.py
@@ -0,0 +1,26 @@
+# Problem: B - Binary Tournament Tree - https://codeforces.com/gym/529269/problem/B
+
+def build_tree(n, sequence):
+    depths = [0] * n
+
+    def backtrack(start, end, depth):
+        if start > end:
+            return
+
+        sub_array = sequence[start : end + 1]
+        max_index = start + sub_array.index(max(sub_array))
+        depths[max_index] = depth
+
+        backtrack(start, max_index - 1, depth + 1)
+        backtrack(max_index + 1, end, depth + 1)
+
+    backtrack(0, n - 1, 0)
+    return depths
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    sequence = list(map(int, input().split()))
+    depths = build_tree(n, sequence)
+    print(" ".join(map(str, depths)))
