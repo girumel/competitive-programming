0de75a326ea7dd6559a9ae92059b1a7d40ce2553
Girum Timerga <girumtim@gmail.com>
2024-06-19 19:25:47 +0300

    Created 'A - Perfect Rectangle' by A2SV Hub

---
+++ A2SV Remote G5 Contest 19 19-Jun-2024/A - Perfect Rectangle 169969.py
@@ -0,0 +1,14 @@
+# Problem: A - Perfect Rectangle - https://codeforces.com/gym/529269/problem/A
+
+from math import isqrt
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    total = sum(a)
+    if isqrt(total) ** 2 == total:
+        print("YES")
+    else:
+        print("NO")
