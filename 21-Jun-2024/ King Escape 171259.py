954efadc97c3b23a22c8b2450d3c4db81d50dfe9
Girum Timerga <girumtim@gmail.com>
2024-06-21 22:30:54 +0300

    Created ' King Escape' by A2SV Hub

---
+++ 21-Jun-2024/ King Escape 171259.py
@@ -0,0 +1,16 @@
+# Problem:  King Escape - https://codeforces.com/problemset/problem/1033/A
+
+n = int(input())
+ax, ay = map(int, input().split())
+bx, by = map(int, input().split())
+cx, cy = map(int, input().split())
+
+top_left = ax < bx and ay < by and ax < cx and ay < cy
+top_right = ax > bx and ay < by and ax > cx and ay < cy
+bottom_right = ax > bx and ay > by and ax > cx and ay > cy
+bottom_left = ax < bx and ay > by and ax < cx and ay > cy
+
+if top_left or top_right or bottom_right or bottom_left:
+    print("YES")
+else:
+    print("NO")
