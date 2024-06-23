f73a495816f1b0dfb9ac71a32fa6e26043390484
Girum Timerga <girumtim@gmail.com>
2024-06-23 22:29:15 +0300

    Created 'Minimal string' by A2SV Hub

---
+++ 23-Jun-2024/Minimal string 171844.py
@@ -0,0 +1,22 @@
+# Problem: Minimal string - https://codeforces.com/contest/797/problem/C
+
+s = input()
+
+t = []
+u = []
+i = 0
+
+for c in map(chr, range(ord("a"), ord("z") + 1)):
+    while t and t[-1] <= c:
+        u.append(t.pop())
+
+    pos = s.find(c, i)
+    while pos >= 0:
+        t.extend(s[i:pos])
+        i = pos + 1
+        u.append(c)
+        pos = s.find(c, i)
+
+u.extend(reversed(t))
+
+print("".join(u))
