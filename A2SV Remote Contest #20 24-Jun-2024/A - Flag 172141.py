aaf5fd10cc85050c4cc2d05c501290e53c4168ea
Girum Timerga <girumtim@gmail.com>
2024-06-24 18:37:33 +0300

    Created 'A - Flag' by A2SV Hub

---
+++ A2SV Remote Contest #20 24-Jun-2024/A - Flag 172141.py
@@ -0,0 +1,19 @@
+# Problem: A - Flag - https://codeforces.com/gym/531416/problem/A
+
+n, m = map(int, input().split())
+flag = [input().strip() for _ in range(n)]
+
+for i in range(n):
+    for j in range(m - 1):
+        if flag[i][j] != flag[i][j + 1]:
+            print("NO")
+            exit()
+# else:
+#     print("YES")
+
+for i in range(m):
+    for j in range(n - 1):
+        if flag[j][i] == flag[j + 1][i]:
+            print("NO")
+            exit()
+print("YES")
