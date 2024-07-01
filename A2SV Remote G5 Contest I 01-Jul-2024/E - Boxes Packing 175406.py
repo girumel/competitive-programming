135ce35d25d6eb4c51d1621b73639a18fa5b49bb
Girum Timerga <girumtim@gmail.com>
2024-07-01 23:32:02 +0300

    Created 'E - Boxes Packing' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 01-Jul-2024/E - Boxes Packing 175406.py
@@ -0,0 +1,9 @@
+# Problem: E - Boxes Packing - https://codeforces.com/gym/500425/problem/E
+
+from collections import Counter
+
+n = int(input())
+boxes = list(map(int, input().split()))
+count = Counter(boxes)
+
+print(max(count.values()))
