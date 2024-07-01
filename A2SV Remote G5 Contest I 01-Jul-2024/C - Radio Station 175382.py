c26494018a2d554db38604251220d88ddd3f8440
Girum Timerga <girumtim@gmail.com>
2024-07-01 22:58:07 +0300

    Created 'C - Radio Station' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 01-Jul-2024/C - Radio Station 175382.py
@@ -0,0 +1,16 @@
+# Problem: C - Radio Station - https://codeforces.com/gym/500425/problem/C
+
+num_servers, num_commands = map(int, input().split())
+servers = {}
+result = []
+
+for _ in range(num_servers):
+    name, ip = input().split()
+    servers[ip] = name
+
+for _ in range(num_commands):
+    command, ip = input().split()
+    ip = ip[:-1]
+    result.append(f"{command} {ip}; #{servers[ip]}")
+
+print("\n".join(result))
