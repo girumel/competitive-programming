7f404d9bf24e7dc61a8f39823c5d70c62eac728f
Girum Timerga <girumtim@gmail.com>
2024-07-01 23:26:16 +0300

    Created 'D - Conveyor Belts' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 01-Jul-2024/D - Conveyor Belts 175401.py
@@ -0,0 +1,20 @@
+# Problem: D - Conveyor Belts - https://codeforces.com/gym/500425/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    n, x1, y1, x2, y2 = map(int, input().split())
+
+    x1 -= 1
+    y1 -= 1
+    x2 -= 1
+    y2 -= 1
+
+    x1 = min(x1, n - x1 - 1)
+    y1 = min(y1, n - y1 - 1)
+    x2 = min(x2, n - x2 - 1)
+    y2 = min(y2, n - y2 - 1)
+
+    min_energy = abs(min(x1, y1) - min(x2, y2))
+    
+    print(min_energy)
