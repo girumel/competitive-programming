b7f76e25a25b9ba1a4d666d13663a4d0f8eceba0
Girum Timerga <girumtim@gmail.com>
2024-07-01 22:58:33 +0300

    Created 'A - In Search of an Easy Problem' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 01-Jul-2024/A - In Search of an Easy Problem 175384.py
@@ -0,0 +1,8 @@
+# Problem: A - In Search of an Easy Problem - https://codeforces.com/gym/500425/problem/A
+
+n = int(input())
+o = list(map(int, input().split()))
+if 1 in o:
+    print("HARD")
+else:
+    print("EASY")
