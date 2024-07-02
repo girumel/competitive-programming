58c9af475daede57a7095bfbf31074219ce0dd73
Girum Timerga <girumtim@gmail.com>
2024-07-02 21:38:12 +0300

    Created 'C - Pairs' by A2SV Hub

---
+++ A2SV Remote G5 Contest II 02-Jul-2024/C - Pairs 175695.py
@@ -0,0 +1,16 @@
+# Problem: C - Pairs - https://codeforces.com/gym/504384/problem/C
+
+from collections import Counter
+
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+
+count_a = Counter(a)
+count_c = Counter(c)
+
+pairs = sum(count_a[num] * count_c[idx + 1] for idx, num in enumerate(b))
+
+print(pairs)
