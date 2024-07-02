58d1bcca159b0086f723f1add1c42b2d927a49f4
Girum Timerga <girumtim@gmail.com>
2024-07-02 22:39:28 +0300

    Created 'E - Design' by A2SV Hub

---
+++ A2SV Remote G5 Contest II 02-Jul-2024/E - Design 175770.py
@@ -0,0 +1,18 @@
+# Problem: E - Design - https://codeforces.com/gym/504384/problem/E
+
+from collections import defaultdict
+
+n, q = map(int, input().split())
+words = input().split()
+word_map = defaultdict(lambda: -1)
+
+for idx, word in enumerate(words):
+    for i in range(1, len(word) + 1):
+        pref = word[:i]
+        for j in range(len(word)):
+            suff = word[j:]
+            word_map[(pref, suff)] = idx
+
+for _ in range(q):
+    pref, suff = input().split()
+    print(word_map[(pref, suff)])
