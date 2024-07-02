d32b46a523a69d3d4b4fcc1e703d8cb95c498fdc
Girum Timerga <girumtim@gmail.com>
2024-07-02 22:07:27 +0300

    Created 'D - Quadruplet' by A2SV Hub

---
+++ A2SV Remote G5 Contest II 02-Jul-2024/D - Quadruplet 175731.py
@@ -0,0 +1,18 @@
+# Problem: D - Quadruplet - https://codeforces.com/gym/504384/problem/D
+
+n, target = map(int, input().split())
+a = list(map(int, input().split()))
+
+pair_sums = {}
+
+for i in range(n):
+    for j in range(i + 1, n):
+        current = a[i] + a[j]
+        if target - current in pair_sums:
+            a_idx, b_idx = pair_sums[target - current]
+            print(a_idx, b_idx, i + 1, j + 1)
+            exit()
+    for j in range(i):
+        pair_sums[a[i] + a[j]] = (j + 1, i + 1)
+
+print("IMPOSSIBLE")
