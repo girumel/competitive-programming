678b2bc75e5b53c642977b55b2d57c0b589737cf
Girum Timerga <girumtim@gmail.com>
2024-07-02 21:10:07 +0300

    Created 'B - File System' by A2SV Hub

---
+++ A2SV Remote G5 Contest II 02-Jul-2024/B - File System 175661.py
@@ -0,0 +1,17 @@
+# Problem: B - File System - https://codeforces.com/gym/504384/problem/B
+
+n = int(input())
+fs = set()
+last = {}
+for i in range(n):
+    name = input()
+    if name not in fs:
+        fs.add(name)
+        last[name] = 0
+        print("OK")
+    else:
+        last[name] += 1
+        new_name = name + str(last[name])
+        fs.add(new_name)
+        last[new_name] = 0
+        print(new_name)
