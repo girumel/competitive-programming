102c525da51b8bb235da507113ba190235da71f5
Girum Timerga <girumtim@gmail.com>
2024-07-05 23:18:25 +0300

    Created 'A - The Battle Of The Five Armies' by A2SV Hub

---
+++ A2SV Remote G5 Contest V 05-Jul-2024/A - The Battle Of The Five Armies 176782.py
@@ -0,0 +1,20 @@
+# Problem: A - The Battle Of The Five Armies - https://codeforces.com/gym/508510/problem/A
+
+capabilities = list(map(int, input().split()))
+soldiers = list(map(int, input().split()))
+
+power_levels = []
+
+for i in range(5):
+    power_level = capabilities[i] * soldiers[i]
+    power_levels.append(power_level)
+    
+first_alliance = sum(power_levels[:3])
+second_alliance = sum(power_levels[3:])
+
+if first_alliance > second_alliance:
+    print("WIN")
+elif first_alliance < second_alliance:
+    print("LOSE")
+else:
+    print("DRAW")
