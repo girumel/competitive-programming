a302d1a26c440870e6ff1d1c31d325c044dad8f7
Girum Timerga <girumtim@gmail.com>
2024-07-05 23:18:42 +0300

    Created 'C - Smeagol's Riddle' by A2SV Hub

---
+++ A2SV Remote G5 Contest V 05-Jul-2024/C - Smeagol's Riddle 176784.py
@@ -0,0 +1,12 @@
+# Problem: C - Smeagol's Riddle - https://codeforces.com/gym/508510/problem/C
+
+n = int(input())
+for _ in range(n):
+    word = input()
+    count = 0
+    
+    for i in range(len(word) // 2):
+        if word[i] != word[-i-1]:
+            count += 1
+    
+    print(count)
