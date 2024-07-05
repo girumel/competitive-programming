d9175251137b0318b8477d91341120c8be0fe881
Girum Timerga <girumtim@gmail.com>
2024-07-05 23:18:38 +0300

    Created 'B - Counting Rings' by A2SV Hub

---
+++ A2SV Remote G5 Contest V 05-Jul-2024/B - Counting Rings 176783.py
@@ -0,0 +1,16 @@
+# Problem: B - Counting Rings - https://codeforces.com/gym/508510/problem/B
+
+shapes = input()
+n = len(shapes)
+
+rings = 0 
+max_rings = 0
+
+for i in range(n):
+    if shapes[i] == 'O':
+        rings += 1
+        max_rings = max(max_rings, rings)
+    else:
+        rings = 0
+ 
+print(max_rings)
