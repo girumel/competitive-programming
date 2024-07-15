c0ad896cc0f4332d46843fa7a82e38b214ad6385
Girum Timerga <girumtim@gmail.com>
2024-07-15 21:25:45 +0300

    Created 'A - Binary Imbalance' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/A - Binary Imbalance 180588.py
@@ -0,0 +1,10 @@
+# Problem: A - Binary Imbalance - https://codeforces.com/gym/535309/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    if "0" in s:
+        print("YES")
+    else:
+        print("NO")
