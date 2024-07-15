b470c3a4e662d76eab88607d2c0caa3a7cb2557b
Girum Timerga <girumtim@gmail.com>
2024-07-15 21:25:53 +0300

    Created 'B - Guess the Maximum' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/B - Guess the Maximum 180589.py
@@ -0,0 +1,12 @@
+# Problem: B - Guess the Maximum - https://codeforces.com/gym/535309/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    max_k = float("inf")
+    for i in range(1, n):
+        max_k = min(max_k, max(a[i - 1], a[i]))
+
+    print(max_k - 1)
