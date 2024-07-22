776828091db5da2ddeb21cedb58b2f8f99c9c073
Girum Timerga <girumtim@gmail.com>
2024-07-22 22:12:39 +0300

    Created 'C - Potions (Hard Version)' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/C - Potions (Hard Version) 182933.py
@@ -0,0 +1,23 @@
+# Problem: C - Potions (Hard Version) - https://codeforces.com/gym/536373/problem/C
+
+import heapq
+
+n = int(input())
+a = list(map(int, input().split()))
+
+heap = []
+health = 0
+potions = 0
+
+for i in range(n):
+    if a[i] + health >= 0:
+        health += a[i]
+        potions += 1
+        if a[i] < 0:
+            heapq.heappush(heap, a[i])
+    elif heap and a[i] > heap[0]:
+        health -= heapq.heappop(heap)
+        heapq.heappush(heap, a[i])
+        health += a[i]
+
+print(potions)
