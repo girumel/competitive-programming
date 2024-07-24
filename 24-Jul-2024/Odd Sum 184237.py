660ea30098637062fedf522af885df06f8c2ce06
Girum Timerga <girumtim@gmail.com>
2024-07-24 20:58:03 +0300

    Created 'Odd Sum' by A2SV Hub

---
+++ 24-Jul-2024/Odd Sum 184237.py
@@ -0,0 +1,20 @@
+# Problem: Odd Sum - https://codeforces.com/problemset/problem/797/B
+
+n = int(input())
+a = list(map(int, input().split()))
+
+min_odd = float("inf")
+is_odd = False
+total = 0
+
+for num in a:
+    if num > 0:
+        total += num
+    if num % 2 != 0:
+        is_odd = True
+        min_odd = min(min_odd, abs(num))
+
+if total % 2 == 0:
+    total -= min_odd
+
+print(total)
