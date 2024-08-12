9fa7697bde6d288db2055da270ca4c6addb54a1e
Girum Timerga <girumtim@gmail.com>
2024-08-12 17:57:45 +0300

    Created 'D - Firaol And His Weapon' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 12-Aug-2024/D - Firaol And His Weapon 190893.py
@@ -0,0 +1,18 @@
+# Problem: D - Firaol And His Weapon - https://codeforces.com/gym/541399/problem/D
+
+n = int(input())
+masses = list(map(int, input().split()))
+
+max_mass = max(masses)
+freq = [0] * (max_mass + 1)
+
+for mass in masses:
+    freq[mass] += 1
+
+dp = [0] * (max_mass + 1)
+dp[1] = freq[1]
+
+for i in range(2, max_mass + 1):
+    dp[i] = max(dp[i - 1], dp[i - 2] + i * freq[i])
+
+print(dp[max_mass])
