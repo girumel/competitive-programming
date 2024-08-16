2673a9ca6cdbf3d3bb83a4b474c651dd912b7db1
Girum Timerga <girumtim@gmail.com>
2024-08-16 21:59:57 +0300

    Created 'E - World is Mine' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 16-Aug-2024/E - World is Mine 192173.py
@@ -0,0 +1,24 @@
+# Problem: E - World is Mine - https://codeforces.com/gym/536373/problem/E
+
+import heapq
+from collections import Counter
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    values = list(map(int, input().split()))
+    counts = list(Counter(values).items())
+
+    counts.sort()
+    max_heap = []
+
+    cakes = 0
+    for i in range(len(counts)):
+        tastiness, count = counts[i]
+        cakes += count
+        heapq.heappush(max_heap, (-count, tastiness))
+
+        if i - len(max_heap) + 1 < cakes:
+            cakes += heapq.heappop(max_heap)[0]
+
+    print(len(counts) - len(max_heap))
