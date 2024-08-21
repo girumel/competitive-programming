69a82997335ad71933546b9c1484ff860d39d3b1
Girum Timerga <girumtim@gmail.com>
2024-08-21 21:28:21 +0300

    Created 'C - Experiment' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 21-Aug-2024/C - Experiment 198464.py
@@ -0,0 +1,24 @@
+# Problem: C - Experiment - https://codeforces.com/gym/537575/problem/C
+
+n = int(input())
+
+experiments = []
+for _ in range(n):
+    s, t, b = map(int, input().split())
+    experiments.append((s, t, b))
+
+max_time = 10004
+room_changes = [0] * (max_time + 1)
+
+for si, ti, bi in experiments:
+    room_changes[si] += bi
+    room_changes[ti + 1] -= bi
+
+max_rooms = 0
+current_rooms = 0
+for change in room_changes:
+    current_rooms += change
+    if current_rooms > max_rooms:
+        max_rooms = current_rooms
+
+print(max_rooms)
