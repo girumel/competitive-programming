6137b6559bfbc7d6b9f6fbc5d05ddcb0aa6f2700
Girum Timerga <girumtim@gmail.com>
2024-08-22 22:36:07 +0300

    Created 'D - Heap Operations' by A2SV Hub

---
+++ A2SV Remote Contest #23 22-Aug-2024/D - Heap Operations 198757.py
@@ -0,0 +1,42 @@
+# Problem: D - Heap Operations - https://codeforces.com/gym/535309/problem/D
+
+import heapq
+
+n = int(input().strip())
+operations = []
+heap = []
+heapq.heapify(heap)
+count = 0
+
+for _ in range(n):
+    line = input().strip().split()
+    cmd = line[0]
+    if cmd == "insert":
+        x = int(line[1])
+        heapq.heappush(heap, x)
+        operations.append((cmd, x))
+    elif cmd == "getMin":
+        x = int(line[1])
+        while heap and heap[0] < x:
+            heapq.heappop(heap)
+            operations.append(("removeMin",))
+            count += 1
+        if not heap or heap[0] > x:
+            heapq.heappush(heap, x)
+            operations.append(("insert", x))
+            count += 1
+        operations.append((cmd, x))
+    elif cmd == "removeMin":
+        if not heap:
+            operations.append(("insert", 1))
+            count += 1
+        else:
+            heapq.heappop(heap)
+        operations.append((cmd,))
+
+print(len(operations))
+for op in operations:
+    if len(op) == 1:
+        print(op[0])
+    else:
+        print(op[0], op[1])
