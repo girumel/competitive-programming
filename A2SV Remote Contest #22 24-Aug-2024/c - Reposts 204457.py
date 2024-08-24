c87d2e12f850a6c8b9aa47a9564092af902eb24d
Girum Timerga <girumtim@gmail.com>
2024-08-24 14:14:55 +0300

    Created 'c - Reposts' by A2SV Hub

---
+++ A2SV Remote Contest #22 24-Aug-2024/c - Reposts 204457.py
@@ -0,0 +1,12 @@
+# Problem: c - Reposts - https://codeforces.com/gym/533316/problem/c
+
+n = int(input())
+reposts = {}
+reposts["polycarp"] = 1
+max_length = 1
+for _ in range(n):
+    a, _, b = input().lower().split()
+    reposts[a] = reposts.get(b, 0) + 1
+    max_length = max(max_length, reposts[a])
+
+print(max_length)
