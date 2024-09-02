104ae6804e462f4ca5ce7a9c1ba8c77ac02936d2
Girum Timerga <girumtim@gmail.com>
2024-09-02 22:51:35 +0300

    Created 'A - Weird Balance' by A2SV Hub

---
+++ A2SV Remote Contest #30 02-Sep-2024/A - Weird Balance 210528.py
@@ -0,0 +1,20 @@
+# Problem: A - Weird Balance - https://codeforces.com/gym/545837/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    ages = list(map(int, input().split()))
+
+    ages.sort(reverse=True)
+    max_age = ages[0]
+
+    i = 1
+    while i < n and ages[i] == max_age:
+        i += 1
+
+    if i == n:
+        print("NO")
+    else:
+        ages[0], ages[i] = ages[i], max_age
+        print("YES")
+        print(*ages)
