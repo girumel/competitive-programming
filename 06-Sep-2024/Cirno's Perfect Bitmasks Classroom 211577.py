1ccf2915bfccb534ae515442a70956f0832892d0
Girum Timerga <girumtim@gmail.com>
2024-09-06 14:20:36 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 06-Sep-2024/Cirno's Perfect Bitmasks Classroom 211577.py
@@ -0,0 +1,9 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    if (x & -x) == x:
+        print(x + 1 + (x == 1))
+    else:
+        print(x & -x)
