ff4677734cd2bf1650aeea0138834803801dc415
Girum Timerga <girumtim@gmail.com>
2024-09-08 22:52:54 +0300

    Created 'Number of Provinces' by A2SV Hub

---
+++ 08-Sep-2024/Number of Provinces 213616.py
@@ -0,0 +1,24 @@
+# Problem: Number of Provinces - https://leetcode.com/problems/number-of-provinces/
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        parent = [i for i in range(n)]
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            root_x = find(x)
+            root_y = find(y)
+            if root_x != root_y:
+                parent[root_x] = root_y
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                if isConnected[i][j] == 1:
+                    union(i, j)
+        
+        return len(set(find(i) for i in range(n)))
