0c70101e3c84b2817c833428d802ceff8d1bcb97
Girum Timerga <girumtim@gmail.com>
2024-09-13 23:35:15 +0300

    Created 'Good Subarrays (Easy Version)' by A2SV Hub

---
+++ 13-Sep-2024/Good Subarrays (Easy Version) 214975.py
@@ -0,0 +1,22 @@
+# Problem: Good Subarrays (Easy Version) - https://codeforces.com/problemset/problem/1736/C1
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    count = 1
+    pairs = 0
+    for num in a:
+        if num >= count:
+            count += 1
+        else:
+            if count > 1:
+                pairs += count * (count - 1) // 2
+                pairs -= num * (num - 1) // 2
+            count = num + 1
+    
+    if count > 1:
+        pairs += count * (count - 1) // 2
+
+    print(pairs)
