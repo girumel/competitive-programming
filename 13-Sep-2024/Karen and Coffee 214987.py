825c03c0c07c94f044b257e578149ad8021ee3ac
Girum Timerga <girumtim@gmail.com>
2024-09-13 23:59:14 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 13-Sep-2024/Karen and Coffee 214987.py
@@ -0,0 +1,36 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n, k, q = map(int, input().split())
+
+recipes = []
+for _ in range(n):
+    l, r = map(int, input().split())
+    recipes.append((l, r))
+
+queries = []
+for _ in range(q):
+    a, b = map(int, input().split())
+    queries.append((a, b))
+
+max_temp = 200000
+temp = [0] * (max_temp + 1)
+for l, r in recipes:
+    temp[l] += 1
+    if r + 1 <= max_temp:
+        temp[r + 1] -= 1
+
+current = 0
+admissible = [0] * (max_temp + 1)
+for i in range(1, max_temp + 1):
+    current += temp[i]
+    if current >= k:
+        admissible[i] = 1
+
+for i in range(1, max_temp + 1):
+    admissible[i] += admissible[i - 1]
+
+results = []
+for a, b in queries:
+    results.append(admissible[b] - admissible[a - 1])
+
+print("\n".join(map(str, results)))
