b1ce4fa62ff65335e45b2aff78ad5a186188d390
Girum Timerga <girumtim@gmail.com>
2024-09-16 23:07:41 +0300

    Created 'A - Divide Array' by A2SV Hub

---
+++ A2SV Remote G5 Contest III 16-Sep-2024/A - Divide Array 215487.py
@@ -0,0 +1,28 @@
+# Problem: A - Divide Array - https://codeforces.com/gym/505936/problem/A
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+positive = []
+negative = []
+zero = []
+
+for num in nums:
+    if num > 0:
+        positive.append(num)
+    elif num < 0:
+        negative.append(num)
+    else:
+        zero.append(num)
+
+if len(negative) > 1:
+    if not positive:
+        positive.extend(negative[1:3])
+        zero.extend(negative[3:])
+    else:
+        zero.extend(negative[1:])
+    negative = negative[:1]
+
+print(len(negative), *negative)
+print(len(positive), *positive)
+print(len(zero), *zero)
