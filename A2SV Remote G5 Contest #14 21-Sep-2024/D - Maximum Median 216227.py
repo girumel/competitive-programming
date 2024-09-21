4ad6d28999c08926cf148b3d99ee4556bb1da504
Girum Timerga <girumtim@gmail.com>
2024-09-21 21:26:31 +0300

    Created 'D - Maximum Median' by A2SV Hub

---
+++ A2SV Remote G5 Contest #14 21-Sep-2024/D - Maximum Median 216227.py
@@ -0,0 +1,20 @@
+# Problem: D - Maximum Median - https://codeforces.com/gym/523225/problem/D
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+a.sort()
+max_median = a[n // 2]
+
+low, high = 1, 2 * 10**9
+while low <= high:
+    mid = (low + high) // 2
+    sum_needed = sum(max(0, mid - a[i]) for i in range(n // 2, n))
+
+    if sum_needed <= k:
+        max_median = max(max_median, mid)
+        low = mid + 1
+    else:
+        high = mid - 1
+
+print(max_median)
