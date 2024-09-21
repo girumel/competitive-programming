e6aa77b8d8744d3b578a31854f8b4619980a9a38
Girum Timerga <girumtim@gmail.com>
2024-09-21 21:42:42 +0300

    Created 'D - City Development' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 21-Sep-2024/D - City Development 216228.py
@@ -0,0 +1,26 @@
+# Problem: D - City Development - https://codeforces.com/gym/520098/problem/D
+
+n = int(input())
+m = list(map(int, input().split()))
+
+current = [0] * (n + 1)
+optimal = [0] * (n + 1)
+max_total = 0
+
+for i in range(1, n + 1):
+    current[i] = m[i - 1]
+    total = m[i - 1]
+
+    for j in range(i - 1, 0, -1):
+        current[j] = min(m[j - 1], current[j + 1])
+        total += current[j]
+
+    for j in range(i + 1, n + 1):
+        current[j] = min(m[j - 1], current[j - 1])
+        total += current[j]
+
+    if total > max_total:
+        max_total = total
+        optimal = current[1:]
+
+print(*optimal)
