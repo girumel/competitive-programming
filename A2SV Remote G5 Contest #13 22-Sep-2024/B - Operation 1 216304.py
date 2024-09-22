fbbd45411df4ba0ff01a23b25e158dbdbe977229
Girum Timerga <girumtim@gmail.com>
2024-09-22 17:04:25 +0300

    Created 'B - Operation 1' by A2SV Hub

---
+++ A2SV Remote G5 Contest #13 22-Sep-2024/B - Operation 1 216304.py
@@ -0,0 +1,11 @@
+# Problem: B - Operation 1 - https://codeforces.com/gym/521860/problem/B
+
+a, m = map(int, input().split())
+
+ops = (a + 1) // 2
+max_ops = ops - a % 2
+
+while ops % m != 0 and ops - (a + 1) // 2 < max_ops:
+    ops += 1
+
+print(ops if ops % m == 0 else -1)
