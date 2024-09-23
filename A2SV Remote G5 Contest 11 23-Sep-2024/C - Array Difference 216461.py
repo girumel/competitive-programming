7fe6d20070254b1ff9a54c96c148e63a80cfdbd9
Girum Timerga <girumtim@gmail.com>
2024-09-23 23:51:11 +0300

    Created 'C - Array Difference' by A2SV Hub

---
+++ A2SV Remote G5 Contest 11 23-Sep-2024/C - Array Difference 216461.py
@@ -0,0 +1,26 @@
+# Problem: C - Array Difference - https://codeforces.com/gym/518838/problem/C
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    a.sort()
+    b.sort(reverse=True)
+    diff = 0
+    switch = -1
+
+    # for i in range(n):
+    #     diff += abs(a[i] - b[i])
+
+    for i in range(n):
+        if abs(a[i] - b[-(n - i)]) > abs(a[i] - b[i]):
+            switch = i
+            break
+        diff += abs(a[i] - b[i])
+
+    if switch != -1:
+        for i in range(switch, n):
+            diff += abs(a[i] - b[-(n - i)])
+    print(diff)
