ca841af2125ad2f841df8b50ae68b5ae7c4f128f
Girum Timerga <girumtim@gmail.com>
2024-09-24 00:00:30 +0300

    Created 'D - Divisible Pairs' by A2SV Hub

---
+++ A2SV Remote G5 Contest 11 23-Sep-2024/D - Divisible Pairs 216468.py
@@ -0,0 +1,20 @@
+# Problem: D - Divisible Pairs - https://codeforces.com/gym/518838/problem/D
+
+t = int(input())
+for _ in range(t):
+    n, x, y = map(int, input().split())
+    array = list(map(int, input().split()))
+    
+    mods = {}
+    count = 0
+    
+    for idx, a in enumerate(array):
+        key = ((x - (a % x)) % x, a % y)
+        if key in mods:
+            count += len(mods[key])
+        mod_key = (a % x, a % y)
+        if mod_key not in mods:
+            mods[mod_key] = set()
+        mods[mod_key].add(idx)
+
+    print(count)
