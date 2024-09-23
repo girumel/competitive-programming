493905601ee713697f4775d3594b588f2fac7bd5
Girum Timerga <girumtim@gmail.com>
2024-09-23 23:36:50 +0300

    Created 'B - Shoe Shuffling' by A2SV Hub

---
+++ A2SV Remote G5 Contest 11 23-Sep-2024/B - Shoe Shuffling 216455.py
@@ -0,0 +1,36 @@
+# Problem: B - Shoe Shuffling - https://codeforces.com/gym/518838/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    sizes = list(map(int, input().split())) + [float("inf")]
+
+    # freq = {}
+    # for size in sizes:
+    #     if size in freq:
+    #         freq[size] += 1
+    #     else:
+    #         freq[size] = 1
+
+    # if any(count == 1 for count in freq.values()):
+    #     print(-1)
+    #     break
+
+    perms = []
+    index = 0
+    group = 1
+    valid = True
+
+    for i in range(1, n + 1):
+        if sizes[i] == sizes[i - 1]:
+            group += 1
+        else:
+            if group == 1:
+                valid = False
+                break
+            perms.append(i)
+            perms.extend(range(index + 1, i))
+            index = i
+            group = 1
+
+    print(*perms) if valid else print("-1")
