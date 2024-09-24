88ea5d14fecb50f23f01c66a32bfe1653307cc9c
Girum Timerga <girumtim@gmail.com>
2024-09-24 19:51:44 +0300

    Created 'E - Allison's Game' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 24-Sep-2024/E - Allison's Game 216551.py
@@ -0,0 +1,20 @@
+# Problem: E - Allison's Game - https://codeforces.com/gym/520098/problem/E
+
+n = int(input())
+heights = list(map(int, input().split()))
+heights.append(0)
+stack = []
+max_length = 0
+
+for i in range(len(heights)):
+    while stack and heights[stack[-1]] >= heights[i]:
+        h = heights[stack.pop()]
+        if not stack:
+            width = i
+        else:
+            width = i - stack[-1] - 1
+        length = min(h, width)
+        max_length = max(max_length, length)
+    stack.append(i)
+
+print(max_length)
