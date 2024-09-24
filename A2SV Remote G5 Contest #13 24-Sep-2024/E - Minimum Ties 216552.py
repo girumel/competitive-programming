5c40f125bab3d50ee46526a91fdff80621e7d57c
Girum Timerga <girumtim@gmail.com>
2024-09-24 19:51:51 +0300

    Created 'E - Minimum Ties' by A2SV Hub

---
+++ A2SV Remote G5 Contest #13 24-Sep-2024/E - Minimum Ties 216552.py
@@ -0,0 +1,16 @@
+# Problem: E - Minimum Ties - https://codeforces.com/gym/521860/problem/E
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    results = []
+    win = (n - 1) // 2
+    results.extend([1] * win)
+    if n % 2 == 0:
+        results.append(0)
+    results.extend([-1] * win)
+    
+    for i in range(1, n + 1):
+        for j in range(n - i):
+            print(results[j], end=" ")
+    print()
