99341299aebe99fe81e7d993e7c37575c592138f
Girum Timerga <girumtim@gmail.com>
2024-09-25 16:07:34 +0300

    Created 'D - Swap Letters' by A2SV Hub

---
+++ A2SV Remote Contest IX 25-Sep-2024/D - Swap Letters 216689.py
@@ -0,0 +1,20 @@
+# Problem: D - Swap Letters - https://codeforces.com/gym/515369/problem/D
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = list(input())
+    
+    ops = 0
+    swapped = set()
+
+    for i in range(n - 1, -1, -1):
+        if s[i] == "A":
+            j = i
+            while j + 1 < n and s[j + 1] == "B" and j not in swapped:
+                # s = s[:j] + "B" + "A" + s[j + 2 :]
+                s[j], s[j + 1] = s[j + 1], s[j]
+                ops += 1
+                swapped.add(j)
+                j += 1
+    print(ops)
