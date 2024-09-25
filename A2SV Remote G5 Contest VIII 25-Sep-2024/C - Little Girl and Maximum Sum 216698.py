daefcf6576fbcf9080af7bcb61e726a185d3221a
Girum Timerga <girumtim@gmail.com>
2024-09-25 16:52:13 +0300

    Created 'C - Little Girl and Maximum Sum' by A2SV Hub

---
+++ A2SV Remote G5 Contest VIII 25-Sep-2024/C - Little Girl and Maximum Sum 216698.py
@@ -0,0 +1,26 @@
+# Problem: C - Little Girl and Maximum Sum - https://codeforces.com/gym/514056/problem/C
+
+n, q = map(int, input().split())
+array = list(map(int, input().split()))
+queries = []
+for _ in range(q):
+    queries.append(tuple(map(int, input().split())))
+
+freq = [0] * (n + 1)
+
+for x, y in queries:
+    freq[x - 1] += 1
+    if y < n:
+        freq[y] -= 1
+
+for i in range(1, n):
+    freq[i] += freq[i - 1]
+
+array.sort(reverse=True)
+freq.sort(reverse=True)
+
+max_sum = 0
+for a, f in zip(array, freq):
+    max_sum += a * f
+
+print(max_sum)
