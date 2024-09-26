ae79873beedca7152c840f7f2aeda85844a57361
Girum Timerga <girumtim@gmail.com>
2024-09-26 21:45:42 +0300

    Created 'D - A Game of Numbers' by A2SV Hub

---
+++ A2SV Remote G5 Contest V 26-Sep-2024/D - A Game of Numbers 216874.py
@@ -0,0 +1,26 @@
+# Problem: D - A Game of Numbers - https://codeforces.com/gym/508510/problem/D
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    frodo = sorted(map(int, input().split()))
+    sam = sorted(map(int, input().split()))
+
+    frodo_l, frodo_r = 0, n - 1
+    sam_l, sam_r = 0, m - 1
+    max_diff = 0
+
+    while frodo_l <= frodo_r:
+        diff_l = abs(frodo[frodo_l] - sam[sam_r])
+        diff_r = abs(frodo[frodo_r] - sam[sam_l])
+
+        if diff_l >= diff_r:
+            max_diff += diff_l
+            frodo_l += 1
+            sam_r -= 1
+        else:
+            max_diff += diff_r
+            frodo_r -= 1
+            sam_l += 1
+
+    print(max_diff)
