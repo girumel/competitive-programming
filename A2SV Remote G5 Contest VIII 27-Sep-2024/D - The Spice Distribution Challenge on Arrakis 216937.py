0b7f29c5b1ba42c5699d61b999a73329e0a820a0
Girum Timerga <girumtim@gmail.com>
2024-09-27 16:56:40 +0300

    Created 'D - The Spice Distribution Challenge on Arrakis' by A2SV Hub

---
+++ A2SV Remote G5 Contest VIII 27-Sep-2024/D - The Spice Distribution Challenge on Arrakis 216937.py
@@ -0,0 +1,19 @@
+# Problem: D - The Spice Distribution Challenge on Arrakis - https://codeforces.com/gym/514056/problem/D
+
+n, m = map(int, input().split())
+spices = list(map(int, input().split()))
+
+current = 0
+counts = {0: 1}
+pairs = 0
+
+for spice in spices:
+    current += spice
+    remainder = current % m
+    if remainder in counts:
+        pairs += counts[remainder]
+        counts[remainder] += 1
+    else:
+        counts[remainder] = 1
+
+print(pairs)
