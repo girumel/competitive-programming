745815b4ce5547b5a2ef984e505ed018c37fb413
Girum Timerga <girumtim@gmail.com>
2024-09-27 17:15:30 +0300

    Created 'E - The Intergalactic Integer Swap' by A2SV Hub

---
+++ A2SV Remote G5 Contest VIII 27-Sep-2024/E - The Intergalactic Integer Swap 216938.py
@@ -0,0 +1,20 @@
+# Problem: E - The Intergalactic Integer Swap - https://codeforces.com/gym/514056/problem/E
+
+from math import gcd
+
+n = int(input())
+coordinates = list(map(int, input().split()))
+
+prefix_gcd = [0] * (n + 1)
+for i in range(1, n + 1):
+    prefix_gcd[i] = gcd(prefix_gcd[i - 1], coordinates[i - 1])
+
+suffix_gcd = [0] * (n + 1)
+for i in range(n - 1, -1, -1):
+    suffix_gcd[i] = gcd(suffix_gcd[i + 1], coordinates[i])
+
+max_gcd = 0
+for i in range(n):
+    max_gcd = max(max_gcd, gcd(prefix_gcd[i], suffix_gcd[i + 1]))
+
+print(max_gcd)
