e22677336e89b7312d50029204253de03ab16784
Girum Timerga <girumtim@gmail.com>
2024-09-27 21:21:30 +0300

    Created 'C - Battleground Permutations' by A2SV Hub

---
+++ A2SV Remote G5 Contest VI 27-Sep-2024/C - Battleground Permutations 216951.py
@@ -0,0 +1,24 @@
+# Problem: C - Battleground Permutations - https://codeforces.com/gym/510902/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    cheifu = list(map(int, input().split()))
+    opponent = list(map(int, input().split()))
+
+    cheifu.sort(reverse=True)
+    opponent.sort(reverse=True)
+
+    arrangements = 1
+    MOD = 10**9 + 7
+    
+    j = 0
+    for i in range(n):
+        while j < n and cheifu[j] > opponent[i]:
+            j += 1
+        arrangements *= j - i
+        arrangements %= MOD
+        if arrangements == 0:
+            break
+
+    print(arrangements)
