d6a1c51c0f1094b541a3bcf734f921ba03cce9ea
Girum Timerga <girumtim@gmail.com>
2024-09-27 20:40:22 +0300

    Created 'D - Sum' by A2SV Hub

---
+++ A2SV Remote G5 Contest VI 27-Sep-2024/D - Sum 216947.py
@@ -0,0 +1,19 @@
+# Problem: D - Sum - https://codeforces.com/gym/510902/problem/D
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    elements = list(map(int, input().split()))
+
+    elements.sort()
+
+    ps = [0] * (n + 1)
+    for i in range(n):
+        ps[i + 1] = ps[i] + elements[i]
+
+    max_sum = 0
+    for i in range(k + 1):
+        cur_sum = ps[n - i] - ps[2 * (k - i)]
+        max_sum = max(max_sum, cur_sum)
+
+    print(max_sum)
