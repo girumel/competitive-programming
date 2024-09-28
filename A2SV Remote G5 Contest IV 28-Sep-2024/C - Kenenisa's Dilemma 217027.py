31d50ba833bc6b68168a71d5ed59b13020bc3ed5
Girum Timerga <girumtim@gmail.com>
2024-09-28 17:20:56 +0300

    Created 'C - Kenenisa's Dilemma' by A2SV Hub

---
+++ A2SV Remote G5 Contest IV 28-Sep-2024/C - Kenenisa's Dilemma 217027.py
@@ -0,0 +1,16 @@
+# Problem: C - Kenenisa's Dilemma - https://codeforces.com/gym/507024/problem/C
+
+n = int(input())
+a = list(map(int, input().split()))
+
+swaps = []
+
+for i in range(n):
+    min_index = min(range(i, n), key=a.__getitem__)
+    if min_index != i:
+        swaps.append((i, min_index))
+        a[i], a[min_index] = a[min_index], a[i]
+
+print(len(swaps))
+for i, j in swaps:
+    print(i, j)
