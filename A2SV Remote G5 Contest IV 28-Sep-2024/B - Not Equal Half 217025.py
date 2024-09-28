ab2a34ce98ac348e0210c3e2a6310deba6cf679b
Girum Timerga <girumtim@gmail.com>
2024-09-28 17:10:58 +0300

    Created 'B - Not Equal Half' by A2SV Hub

---
+++ A2SV Remote G5 Contest IV 28-Sep-2024/B - Not Equal Half 217025.py
@@ -0,0 +1,14 @@
+# Problem: B - Not Equal Half - https://codeforces.com/gym/507024/problem/B
+
+n = int(input())
+a = list(map(int, input().split()))
+
+a.sort()
+
+first_half = sum(a[:n])
+last_half = sum(a[n:])
+
+if first_half == last_half:
+    print(-1)
+else:
+    print(*a)
