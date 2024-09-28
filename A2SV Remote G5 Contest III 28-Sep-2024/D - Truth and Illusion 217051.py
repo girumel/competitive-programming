947bd02884887e7bb25f5a4298b2be71830446b3
Girum Timerga <girumtim@gmail.com>
2024-09-28 21:05:34 +0300

    Created 'D - Truth and Illusion' by A2SV Hub

---
+++ A2SV Remote G5 Contest III 28-Sep-2024/D - Truth and Illusion 217051.py
@@ -0,0 +1,22 @@
+# Problem: D - Truth and Illusion - https://codeforces.com/gym/505936/problem/D
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    grid = [list(map(int, input())) for _ in range(n)]
+
+    operations = 0
+    
+    for i in range((n + 1) // 2):
+        for j in range(n // 2):
+            cells = [
+                (i, j),
+                (n - 1 - j, i),
+                (n - 1 - i, n - 1 - j),
+                (j, n - 1 - i),
+            ]
+            ones = sum(grid[x][y] for x, y in cells)
+            zeros = 4 - ones
+            operations += min(ones, zeros)
+    
+    print(operations)
