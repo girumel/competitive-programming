c6d72d907bae58394a3420a05ccf453eacd35f17
Girum Timerga <girumtim@gmail.com>
2024-09-28 20:44:08 +0300

    Created 'B - Firaols Contest' by A2SV Hub

---
+++ A2SV Remote G5 Contest III 28-Sep-2024/B - Firaols Contest 217046.py
@@ -0,0 +1,23 @@
+# Problem: B - Firaols Contest - https://codeforces.com/gym/505936/problem/B
+
+n, m = map(int, input().split())
+difficulties = list(map(int, input().split()))
+
+counts = [0] * (n + 1)
+unique = set()
+result = []
+
+for difficulty in difficulties:
+    counts[difficulty] += 1
+    unique.add(difficulty)
+
+    if len(unique) == n:
+        result.append(1)
+        for i in range(1, n + 1):
+            counts[i] -= 1
+            if counts[i] == 0:
+                unique.remove(i)
+    else:
+        result.append(0)
+
+print("".join(map(str, result)))
