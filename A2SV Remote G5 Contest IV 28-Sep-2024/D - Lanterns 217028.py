542e7ce1352a6debb622c01b42136a739c4948f0
Girum Timerga <girumtim@gmail.com>
2024-09-28 17:35:47 +0300

    Created 'D - Lanterns' by A2SV Hub

---
+++ A2SV Remote G5 Contest IV 28-Sep-2024/D - Lanterns 217028.py
@@ -0,0 +1,20 @@
+# Problem: D - Lanterns - https://codeforces.com/gym/507024/problem/D
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    lanterns = [tuple(map(int, input().split())) for _ in range(n)]
+
+    lanterns.sort(key=lambda x: (x[0], -x[1]))
+
+    index, count, total = 0, 0, 0
+    for i in range(len(lanterns)):
+        if i < index:
+            continue
+        total += lanterns[i][1]
+        count += 1
+        while index < len(lanterns) and lanterns[index][0] <= count:
+            index += 1
+        count = max(0, i - index + 1)
+
+    print(total)
